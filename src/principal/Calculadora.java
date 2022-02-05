be0b51cab881331f81659f5f5a0602f19cc5ea89
d4v1z <ooo.sporte@gmail.com>
2022-02-05 01:00:42 +0100

    Carga packages y clase Claculadora(Main)

---
+++ src/principal/Calculadora.java
@@ -0,0 +1,14 @@
+package principal;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		/*
+		 * Aqui vamos a hacer las llamdas de los metodos de las clases
+		 * individuales
+		 * 
+		 */
+
+	}
+
+}
