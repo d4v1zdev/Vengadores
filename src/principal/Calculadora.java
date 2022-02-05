512c909df8111f156188fef9c7885c9b4f8feb82
d4v1z <95466656+d4v1zdev@users.noreply.github.com>
2022-02-05 20:03:12 +0100

    Se tabula correctamente la clase Cociente
    Se tabula ya que habia quedado descuadrada con la anterior

--- src/principal/Calculadora.java
+++ src/principal/Calculadora.java
@@ -16,13 +16,13 @@ public class Calculadora {
 		
 		Resta r1 = new Resta();
 		
-		System.out.println("** RESTA DE TRES NÚMEROS REALES");
+		System.out.println("** RESTA DE TRES NÃšMEROS REALES");
 		System.out.println("El resultado de la resta de 9,3 - 2,1 - 2,9 es: " + Math.floor(r1.restaTresD(9.3, 2.1, 2.9)));
 		
-		System.out.println("** RESTA DE DOS NÚMEROS REALES");
+		System.out.println("** RESTA DE DOS NÃšMEROS REALES");
 		System.out.println("El resultado de la resta de 23,4 - 7,3 es: " + Math.floor(r1.restaDosD(23.4, 7.3)));
 		
-		System.out.println("** RESTA DE DOS NÚMEROS ENTEROS");
+		System.out.println("** RESTA DE DOS NÃšMEROS ENTEROS");
 		System.out.println("El resultado de la resta de 15 - 4  es: " + r1.restaDosI(15, 4));
 		
 		
@@ -32,29 +32,29 @@ public class Calculadora {
 		// LLAMADAS METODOS COCIENTE
 		
 		Cociente c1 = new Cociente();
-        System.out.println("La division de 2 numeros enteros es: " +c1.cocEnterosTry(6,4));
-        System.out.println("** DIVISION NUMEROS REALES **");
-        try {
-            System.out.println("La division de 2 numeros REALES es.: " + c1.cocReal(7.34,0));
-        }catch(Exception e) {
-            System.out.println("No se permite la division por 0");
-            System.out.println("El tipo de excepcion Java es: " + e.toString());
-        }
-        System.out.println("** CALCULO DEL INVERSO **");
-        try {
-            System.out.println("El numero inverso es.: " + c1.inverso(7.32));
-        }catch(Exception e) {
-            System.out.println("El numero 0 no tiene Inverso");
-            System.out.println("El tipo de excepcion Java es: " + e.toString());
-        }
+        	System.out.println("La division de 2 numeros enteros es: " +c1.cocEnterosTry(6,4));
+        	System.out.println("** DIVISION NUMEROS REALES **");
+        	try {
+            		System.out.println("La division de 2 numeros REALES es.: " + c1.cocReal(7.34,0));
+        	}catch(Exception e) {
+            		System.out.println("No se permite la division por 0");
+            		System.out.println("El tipo de excepcion Java es: " + e.toString());
+		}
+        	System.out.println("** CALCULO DEL INVERSO **");
+        	try {
+            		System.out.println("El numero inverso es.: " + c1.inverso(7.32));
+        	}catch(Exception e) {
+            		System.out.println("El numero 0 no tiene Inverso");
+            		System.out.println("El tipo de excepcion Java es: " + e.toString());
+        	}
 
-        System.out.println("** CALCULO DE LA RAIZ DE UN NUMERO **");
-        try {
-            System.out.println("la raiz cuadrada es.: " + c1.calRaiz(-1));
-        }catch(Exception e) {
-            System.out.println("No es posible calcular la raiz de un numero negativo");
-            System.out.println("El tipo de excepcion Java es: " + e.toString());
-        }
+        	System.out.println("** CALCULO DE LA RAIZ DE UN NUMERO **");
+        	try {
+            		System.out.println("la raiz cuadrada es.: " + c1.calRaiz(-1));
+        	}catch(Exception e) {
+            		System.out.println("No es posible calcular la raiz de un numero negativo");
+            		System.out.println("El tipo de excepcion Java es: " + e.toString());
+        	}
 
 
 	}
