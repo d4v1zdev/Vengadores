41f7e00f6b18b050224b0b7e9c233d2d224b7fbf
simon.patrick <simonpatrickreyes@gmail.com>
2022-02-10 01:47:07 +0100

    Incluyo mi clase Suma con sus metodos.

---
+++ src/clases_indiv/Suma.java
@@ -0,0 +1,90 @@
+package calculadora_main;
+
+public class Suma {
+
+	/**
+	 * Declaracion de la clase <b>Suma</b>, operacion aritmetica que consiste en
+	 * reunir varias cantidades en una sola; se representa con el signo <b>+</b>.
+	 * 
+	 * @author simon
+	 * @version 1.0
+	 */
+
+	private int resultado;
+
+	/**
+	 * Metodo 1: Suma de dos numeros reales <b>(double)</b>, tendra 2 parametros de
+	 * entrada <b>(num1,num2)</b> y uno de salida que sera <b>resultado</b> (suma de
+	 * los valores). El orden en el que los valores se introducen no altera el
+	 * resultado. <b>Rango aproximado de 15 cifras decimales</b>.
+	 * 
+	 * @param num1 primer sumando o valor.
+	 * @param num2 segundo sumando o valor.
+	 * @return <b>Resultado</b>: Valor de la <b>suma</b> de los valores <b>num1</b>
+	 *         y <b>num2</b>.
+	 */
+	public double sumaReales(double num1, double num2) {
+
+		double resultado = (num1 + num2);
+		return resultado;
+	}
+
+	/**
+	 * Metodo 2: Suma de dos numeros enteros <b>(int)</b>, tendra 2 parametros de
+	 * entrada <b>(num1,num2)</b> y uno de salida que sera <b>resultado</b> (suma de
+	 * los valores). El orden en el que los valores se introducen no altera el
+	 * resultado. Rango de la variable <b>int</b>: <b>-2.147.483.648 a
+	 * 2.147.483.647</b>.
+	 * 
+	 * @param num1 primer sumando o valor.
+	 * @param num2 segundo sumando o valor.
+	 * @return <b>Resultado</b>: Valor de la <b>suma</b> de los valores <b>num1</b>
+	 *         y <b>num2</b>.
+	 */
+	public int sumaEnt(int num1, int num2) {
+
+		int resultado = (num1 + num2);
+		return resultado;
+	}
+
+	/**
+	 * Metodo 3: Suma de tres numeros reales <b>(double)</b>, tendra 3 parametros de
+	 * entrada <b>(num1,num2,num3)</b> y uno de salida que sera <b>resultado</b>
+	 * (suma de los valores). El orden en el que los valores se introducen no altera
+	 * el resultado. <b>Rango aproximado de 15 cifras decimales</b>.
+	 * 
+	 * @param num1 primer sumando o valor.
+	 * @param num2 segundo sumando o valor.
+	 * @param num3 tercer sumando o valor.
+	 * @return <b>Resultado</b>: Valor de la <b>suma</b> de los valores <b>num1</b>,
+	 *         <b>num2</b> y <b>num3</b>.
+	 */
+	public double sumaRealesTrio(double num1, double num2, double num3) {
+		double resultado = (num1 + num2 + num3);
+		return resultado;
+	}
+
+	/**
+	 * Metodo 4: Suma de numeros enteros <b>(int)</b> con valor acumulado, tendra un
+	 * parametro de entrada y la clase debera de guardar el valor acumulado. Rango
+	 * de la variable <b>int</b>: <b>-2.147.483.648 a 2.147.483.647</b>.
+	 * 
+	 * @param num sumando o valor que ira añadiendose al valor <b>resultado</b>.
+	 * @return <b>Resultado</b>: Valor acumulado que sera la suma total de los
+	 *         valores <b>resultado</b> y <b>num</b>.
+	 * @throws Exception Si el valor supera el rango permitido
+	 *                   (<b>-2.147.483.648/2.147.483.647</b>), generaria una
+	 *                   excepcion de tipo <b>java.lang.exception</b>.
+	 */
+	public int sumaAcumulada(int num) {
+		resultado = resultado + num;
+		return resultado;
+
+	}
+
+	public String getResultado() {
+		return "La suma total es: " + resultado;
+
+	}
+
+}
--- src/principal/Calculadora.java
+++ src/principal/Calculadora.java
@@ -10,52 +10,59 @@ public class Calculadora {
 	 * Realizamos las llamdas a los metodos de cada clase
 	 */
 	public static void main(String[] args) {
+
 		// LLAMADAS METODOS SUMA
+
+		Suma c = new Suma();
+
+		System.out.println("Suma de dos numeros reales");
+		c.sumaReales(23, 99);
+		c.sumaReales(77, 01);
+		System.out.println(c.getResultado());
+		
 		
 		// LLAMADAS METODOS RESTA
-		
+
 		Resta r1 = new Resta();
-		
+
 		System.out.println("** RESTA DE TRES NÚMEROS REALES");
-		System.out.println("El resultado de la resta de 9,3 - 2,1 - 2,9 es: " + Math.floor(r1.restaTresD(9.3, 2.1, 2.9)));
-		
+		System.out
+				.println("El resultado de la resta de 9,3 - 2,1 - 2,9 es: " + Math.floor(r1.restaTresD(9.3, 2.1, 2.9)));
+
 		System.out.println("** RESTA DE DOS NÚMEROS REALES");
 		System.out.println("El resultado de la resta de 23,4 - 7,3 es: " + Math.floor(r1.restaDosD(23.4, 7.3)));
-		
+
 		System.out.println("** RESTA DE DOS NÚMEROS ENTEROS");
 		System.out.println("El resultado de la resta de 15 - 4  es: " + r1.restaDosI(15, 4));
-		
-		
-		
+
 		// LLAMADAS METODOS PRODUCTO
-		
+
 		// LLAMADAS METODOS COCIENTE
-		
+
 		Cociente c1 = new Cociente();
-        	System.out.println("La division de 2 numeros enteros es: " +c1.cocEnterosTry(6,4));
-        	System.out.println("** DIVISION NUMEROS REALES **");
-        	try {
-            		System.out.println("La division de 2 numeros REALES es.: " + c1.cocReal(7.34,0));
-        	}catch(Exception e) {
-            		System.out.println("No se permite la division por 0");
-            		System.out.println("El tipo de excepcion Java es: " + e.toString());
+		System.out.println("La division de 2 numeros enteros es: " + c1.cocEnterosTry(6, 4));
+		System.out.println("** DIVISION NUMEROS REALES **");
+		try {
+			System.out.println("La division de 2 numeros REALES es.: " + c1.cocReal(7.34, 0));
+		} catch (Exception e) {
+			System.out.println("No se permite la division por 0");
+			System.out.println("El tipo de excepcion Java es: " + e.toString());
 		}
-        	System.out.println("** CALCULO DEL INVERSO **");
-        	try {
-            		System.out.println("El numero inverso es.: " + c1.inverso(7.32));
-        	}catch(Exception e) {
-            		System.out.println("El numero 0 no tiene Inverso");
-            		System.out.println("El tipo de excepcion Java es: " + e.toString());
-        	}
+		System.out.println("** CALCULO DEL INVERSO **");
+		try {
+			System.out.println("El numero inverso es.: " + c1.inverso(7.32));
+		} catch (Exception e) {
+			System.out.println("El numero 0 no tiene Inverso");
+			System.out.println("El tipo de excepcion Java es: " + e.toString());
+		}
 
-        	System.out.println("** CALCULO DE LA RAIZ DE UN NUMERO **");
-        	try {
-            		System.out.println("la raiz cuadrada es.: " + c1.calRaiz(-1));
-        	}catch(Exception e) {
-            		System.out.println("No es posible calcular la raiz de un numero negativo");
-            		System.out.println("El tipo de excepcion Java es: " + e.toString());
-        	}
-
+		System.out.println("** CALCULO DE LA RAIZ DE UN NUMERO **");
+		try {
+			System.out.println("la raiz cuadrada es.: " + c1.calRaiz(-1));
+		} catch (Exception e) {
+			System.out.println("No es posible calcular la raiz de un numero negativo");
+			System.out.println("El tipo de excepcion Java es: " + e.toString());
+		}
 
 	}
 
